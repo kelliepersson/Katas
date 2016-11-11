761363363021400b3d8809147a24ba7acf06c467
Kellie Spears <kelliespears@gmail.com>
2016-11-10 19:06:29 -0800

    Initial Commit

---
+++ Katas/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Katas
+//
+//  Created by Kellie Spears on 11/10/16.
+//  Copyright © 2016 Kellie Spears. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
